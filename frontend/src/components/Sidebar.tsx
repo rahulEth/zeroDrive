4fce393faab7b24c5e7f2fd3726889ecf4070f10
Nadiia Balaian <nadiia.balaian@gmail.com>
2024-09-21 19:20:07 +0800

    filter and sort documents

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { ROUTES } from "../constants/routes";
 
 export const Sidebar = () => {
@@ -6,11 +6,20 @@ export const Sidebar = () => {
 		<aside className="w-64 bg-gray-800 text-white flex flex-col">
 			<nav className="flex-1 px-4 py-4 space-y-4">
 				<h2 className="text-accent font-bold">Your data - your control</h2>
-				<h5 className="text-primary-dark">Trustless and Non-Custodial security </h5>
+				<h5 className="text-primary-dark">
+					Trustless and Non-Custodial security
+				</h5>
 				<ul>
 					{ROUTES.map((route) => (
 						<li key={route.path} className="py-3 border-b-2">
-							<Link to={route.path}>{route.name}</Link>
+							<NavLink
+								to={route.path}
+								className={({ isActive }) =>
+									isActive ? "text-accent font-bold" : ""
+								}
+							>
+								{route.name}
+							</NavLink>
 						</li>
 					))}
 				</ul>
